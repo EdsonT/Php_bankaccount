da25bb43ef160c81d0702842f0c10ee0bb552267
EdsonT <ednoosde.wob@gmail.com>
2018-07-11 18:42:44 -0400

    new file:   application.php 	new file:   classes/employee.php 	new file:   classes/person.php

---
+++ abstract_person/application.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once 'classes/employee.php';
+
+$e=new Employee('Jhon','Doe',99999);
+echo $e;
---
+++ abstract_person/classes/employee.php
@@ -0,0 +1,17 @@
+<?php
+require_once 'person.php';
+class Employee extends Person{
+	private $salary;
+
+	public function __construct($firstName,$lastName,$salary){
+		parent::__construct($firstName, $lastName);
+		$this->salary=$salary;
+	}
+	public function getSalary(){
+		return $this->salary;
+	}
+	public function __toString(){
+		return sprintf("Name: %s<br> Salary $%0.2f",parent::__toString(), $this->getSalary());
+	}
+
+}
