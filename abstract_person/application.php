435b8016440a6a136a625a386198d4f740d2b4e4
EdsonT <ednoosde.wob@gmail.com>
2018-07-11 19:18:46 -0400

    modified:   application.php 	new file:   classes/contractor.php

--- abstract_person/application.php
+++ abstract_person/application.php
@@ -1,6 +1,12 @@
 <?php
-
+echo "<br/>------ EMPLOYEE:<br/>";
 require_once 'classes/employee.php';
-
+require_once 'classes/contractor.php';
 $e=new Employee('Jhon','Doe',99999);
 echo $e;
+echo "<br/>------ CONTRACTOR:<br/>";
+$c= new Contractor ('David','Hill',75,1750);
+echo $c;
+
+
+
